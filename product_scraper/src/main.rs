40895677713c5bc64faa7d0fd8ebb4f78d5f2603
nioe <eoinmitchell39@proton.me>
2025-04-22 23:09:26 -0400

    init commit, trying rust and python web scraping tools

---
+++ product_scraper/src/main.rs
@@ -0,0 +1,57 @@
+use fantoccini::{ClientBuilder, Locator};
+use serde_json::json;
+
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub async fn default_client() -> Result<fantoccini::Client> {
+
+    // this is how I passed the stockX captia   
+    // removes default headless chrome flags
+    let mut caps = serde_json::Map::new();
+    caps.insert(
+        "goog:chromeOptions".to_string(),
+        json!({
+            "args": [
+                "--headless",
+                "--disable-gpu",
+                "--disable-blink-features=AutomationControlled",
+                "--no-sandbox",
+                "--window-size=1920,1080"
+            ],
+            "excludeSwitches": ["enable-automation"],
+            "useAutomationExtension": false
+        }),
+    );
+    let c = ClientBuilder::native()
+        .capabilities(caps.into())
+        .connect("http://localhost:4444")
+        .await
+        .expect("failed to connect to WebDriver");
+
+    // when a service checks if the browser is a web driver it calls this function instead of the google one
+    let script = r#"
+      Object.defineProperty(navigator, 'webdriver', {
+        get: () => false
+      });
+    "#;
+    c.execute(script, Vec::new()).await?;
+
+    Ok(c)
+}
+
+
+
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let c = default_client().await?;
+
+    c.goto("https://stockx.com/air-jordan-4-retro-sb-navy").await?;
+    let text = c.find(Locator::Css(r#"h2[data-testid="trade-box-buy-amount"]"#)).await?.text().await?;
+    println!("{}", text);
+
+    Ok(())
+}
