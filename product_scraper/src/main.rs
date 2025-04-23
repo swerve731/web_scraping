cb2c0cbc1d5fdf7a82bfb4d401ea0749c0835e32
nioe <eoinmitchell39@proton.me>
2025-04-23 09:49:51 -0400

    fix limit one search

--- product_scraper/src/main.rs
+++ product_scraper/src/main.rs
@@ -7,7 +7,7 @@ use product_scraper::scrapers;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let products = scrapers::stockx::StockxScraper::search_scrape("jordans".into(), 1000).await?;
+    let products = scrapers::stockx::StockxScraper::search_scrape("jordans".into(), 30).await?;
 
     println!("{:?}", products);
     Ok(())
